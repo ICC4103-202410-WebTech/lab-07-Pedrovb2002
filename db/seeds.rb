0b5c80df3e6f4ce8aeaeea518a80e6f3dbd0ae68
pedrovb2002 <pedrovb2002@LenovoPedroV>
2024-04-26 17:31:55 -0400

    fin

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,123 @@
 #   end
 Post.delete_all
 User.delete_all
+
+User.create(
+  name: 'Pedro Valenzuela',
+  email: 'correopv@ejemplo.com',
+  password: '12345678'
+)
+
+User.create(
+  name: 'Juan Perez',
+  email: 'correojp@ejemplo.com',
+  password: 'password'
+)
+
+User.create(
+  name: 'Clara Mate',
+  email: 'correocm@ejemplo.com',
+  password: '87654321'
+)
+
+User.create(
+  name: 'Lucas Rata',
+  email: 'correolr@ejemplo.com',
+  password: '00000001'
+)
+
+User.create(
+  name: 'Max Toledo',
+  email: 'correomt@ejemplo.com',
+  password: '123654789'
+)
+
+Post.create(
+  title: 'Post primero',
+  content: 'Foto playa',
+  published_at: Time.current,
+  user_id: 1
+)
+
+Post.create(
+  title: 'Post segundo',
+  content: 'Texto de saludos',
+  published_at: Time.current,
+  user_id: 2
+)
+
+Post.create(
+  title: 'Post tercero',
+  content: 'Texto de reclamo',
+  published_at: Time.current,
+  user_id: 3
+)
+
+Post.create(
+  title: 'Post cuarto',
+  content: 'Foto perro',
+  published_at: Time.current,
+  user_id: 4
+)
+
+Post.create(
+  title: 'Post quinto',
+  content: 'Foto casa',
+  published_at: Time.current,
+  user_id: 5
+)
+
+Post.create(
+  title: 'Post sexto',
+  content: 'Foto comida',
+  published_at: Time.current,
+  user_id: 1
+)
+
+Post.create(
+  title: 'Post septimo',
+  content: 'Texto de despedida',
+  published_at: Time.current,
+  user_id: 2
+)
+
+Post.create(
+  title: 'Post octavo',
+  content: 'Texto de feliz',
+  published_at: Time.current,
+  user_id: 3
+)
+
+Post.create(
+  title: 'Post noveno',
+  content: 'Foto solo',
+  published_at: Time.current,
+  user_id: 4
+)
+
+Post.create(
+  title: 'Post decimo',
+  content: 'Foto saltando',
+  published_at: Time.current,
+  user_id: 5
+)
+
+Tag.create(
+  name: 'Tag 1'
+)
+
+Tag.create(
+  name: 'Tag 2'
+)
+
+Tag.create(
+  name: 'Tag 3'
+)
+
+Tag.create(
+  name: 'Tag 4'
+)
+
+Tag.create(
+  name: 'Tag 5'
+)
